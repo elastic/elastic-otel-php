d5983d1f1530cb5ded1bd3a73921e9d2a0d9beae
Pawel Filipczak <pawel.filipczak@elastic.co>
2024-06-04 21:00:00 +0200

    try to change userid

--- tools/build/build_native.sh
+++ tools/build/build_native.sh
@@ -79,4 +79,5 @@ docker run --rm -t $USERID -v ${PWD}:/source \
     ${CONAN_USER_HOME_MP} \
     -w /source/prod/native \
     elasticobservability/apm-agent-php-dev:native-build-gcc-12.2.0-${BUILD_ARCHITECTURE}-0.0.2 \
-    sh -c "id && echo CONAN_USER_HOME=\$CONAN_USER_HOME && ${CONFIGURE} cmake --build --preset ${BUILD_ARCHITECTURE}-release -j${NCPU} && ctest --preset ${BUILD_ARCHITECTURE}-release --verbose"
+    sh -c "usermod -u $(id -u) build && id && echo CONAN_USER_HOME=\$CONAN_USER_HOME && ${CONFIGURE} cmake --build --preset ${BUILD_ARCHITECTURE}-release -j${NCPU} && ctest --preset ${BUILD_ARCHITECTURE}-release --verbose"
+
