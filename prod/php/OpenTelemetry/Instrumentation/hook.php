265eb9706b905ade30c8cd357f035d35ddecd0e5
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2024-09-09 13:58:31 +0300

    Added missing file header header with the license

--- prod/php/OpenTelemetry/Instrumentation/hook.php
+++ prod/php/OpenTelemetry/Instrumentation/hook.php
@@ -1,6 +1,29 @@
 <?php
 
-/** @noinspection PhpInternalEntityUsedInspection */
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * @noinspection PhpInternalEntityUsedInspection
+ */
+
+declare(strict_types=1);
 
 namespace OpenTelemetry\Instrumentation;
 
@@ -15,6 +38,7 @@ use Elastic\OTel\InstrumentationBridge;
  * Called by the OTel instrumentations
  *
  * @noinspection PhpUnused
+ * @noinspection PhpInternalEntityUsedInspection
  */
 
 /**
