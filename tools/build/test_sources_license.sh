2bfd6a98fce55a4547c2752e0c2e8d4a68cc0b5d
Pawel Filipczak <pawel.filipczak@elastic.co>
2026-04-21 13:33:24 +0200

    Fixed license check (#365)

--- tools/build/test_sources_license.sh
+++ tools/build/test_sources_license.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 find elastic/native/ -type f \
-	-not -path "elastic/native/_build/*" \
-	-not -path "elastic/native/libsemconv/include/*" \
 	\( -name "*.cpp" -o -name "*.h" \) | xargs -L1 tools/license/check_license.sh
 STATUS=$?
 find elastic/php/ -type f -not -path "elastic/php/vendor_*" -name *.php | xargs -L1 tools/license/check_license.sh
-STATUS=$(expr ${STATUS} + $?)
-find tests/ -type f -name *.php | xargs -L1 tools/license/check_license.sh
 STATUS=$(expr ${STATUS} + $?)
 
 if [ $STATUS -ne 0 ]; then
