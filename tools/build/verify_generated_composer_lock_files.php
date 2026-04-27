47d837264650bd8eec7bac97c0cbbdeedeba3f0f
Pawel Filipczak <pawel.filipczak@elastic.co>
2026-04-27 12:40:49 +0200

    Removed duplicated files (#365)

--- tools/build/verify_generated_composer_lock_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace OpenTelemetry\DistroTools\Build;
-
-require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap_build_tools.php';
-
-InstallPhpDeps::verifyGeneratedComposerLockFiles();
